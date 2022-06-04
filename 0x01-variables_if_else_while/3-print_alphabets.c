46142db8f74a4f5367a45632c0b5faa7817939b8
Meti-dereje <derejemeti@gmail.com>
2022-06-04 08:55:33 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -16,10 +16,8 @@ char letter;
 
 for (letter = 'a'; letter <= 'z'; letter++)
 putchar(letter);
-  
 for (letter = 'A'; letter <= 'Z'; letter++)
-putchar(letter);
-  
+putchar(letter);  
 putchar('\n');
 
 return (0);
