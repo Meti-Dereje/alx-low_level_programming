44a259fa33b88efb6634c994aa9b264ad6021c3b
Meti-dereje <derejemeti@gmail.com>
2022-06-05 00:52:59 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+/*
+* File: 102-print_comb5.c
+* Auth: Brennan D Baraban
+*/
+
+#include<stdio>
+
+/**
+* main - Prints all possible combinations of two two-digit numbers,
+*        ranging from 0-99, separated by a comma followed by a space.
+*
+* Return: Always 0.
+*/
+
+int main(void)
+{
+int n1, n2;
+for (n1=0, n1<=98, n1++);
+{
+for (n2=0, n2<=99, n2++)
+{
+putchar((num1 / 10) + '0');
+putchar((n1 % 10) + '0');
+putchar(' ');
+putchar((n2 / 10) + '0');
+putchar((n2 % 10) + '0');
+if (n1 == 98 && n2 == 99)
+continue;    
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+
+return (0);
+}
