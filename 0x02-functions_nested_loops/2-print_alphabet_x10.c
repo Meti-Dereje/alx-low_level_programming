9f6cf2effd910917e5bf09bddc168266ebc1e403
Meti-dereje <derejemeti@gmail.com>
2022-06-07 07:42:58 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
-*print_alphabet_x10 - Prints 10 times the alphabet, in lowercase. 
+* print_alphabet_x10 - Prints 10 times the alphabet, in lowercase. 
 */
 void print_alphabet_x10(void)
-
 {
 int c = 0;
 char l;
