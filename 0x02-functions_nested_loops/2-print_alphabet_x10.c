23a16181dfd8c204d5a05c1e627c9bd908b8129a
Meti-dereje <derejemeti@gmail.com>
2022-06-06 23:16:45 -0700

    10 times alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* 
+*main print alphabet 10 times
+*/
+void print_alphabet_x10(void);
+
+{
+int c = 0;
+char l;
+while (c <= 9)
+{
+for (l = 'a'; l <= 'z'; l++)
+_putchar(l);
+_putchar('\n');
+}
+
+}
