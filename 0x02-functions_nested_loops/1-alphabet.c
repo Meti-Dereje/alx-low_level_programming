2882a401766c506078e5ff4ca09c5889a5665007
Meti-dereje <derejemeti@gmail.com>
2022-06-07 07:03:38 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,8 +2,8 @@
 
 /**
 * main - check the code
-*
- Return: Always 0.
+*print_alphabet - prints the alphabet in lowercase, followed by a new line
+* Return: Always 0.
 */
 void print_alphabet(void)
 {
