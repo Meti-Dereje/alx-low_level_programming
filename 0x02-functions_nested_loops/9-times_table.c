09d901d1d811e4ae368cd1dcf85559427079eb5d
Meti-dereje <derejemeti@gmail.com>
2022-06-07 11:00:02 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,20 +5,20 @@
 */
 void times_table(void)
 {
-int n, M, A;
-for (n = 0; n <=9; n++)
+int num, mult, prod;
+for (num = 0; num <=9; num++)
 _putchar('0');
 {
-for (M = 1; M <= 9; M++)
+for (mult = 1; mult <= 9; mult++)
 {
 _putchar(',');
 _putchar(' ');
-A = n * M;
-if (A <= 9)
+prod = num * mult;
+if (prod <= 9)
 _putchar(' ');
 else
-_putchar((A / 10) + '0');
-_putchar((A % 10) + '0');
+_putchar((prod / 10) + '0');
+_putchar((prod % 10) + '0');
 }
 _putchar('\n');
 }
