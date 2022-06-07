e08c67cb5b2106a95dde84357c3d2e7f1d8a42a5
Meti-dereje <derejemeti@gmail.com>
2022-06-07 10:38:23 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,7 +3,7 @@
 *jack_bauer - Prints every minute of the day of
 *              Jack Bauer, starting from 00:00 to 23:59.
 */
-int main(void)
+void jack_bauer(void)
 {
 int H, M;
 for (H = 0; H <= 23; H++)
