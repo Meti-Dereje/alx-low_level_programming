0e65832810eaf7aae01848ac55a67a313335e3a3
Meti-dereje <derejemeti@gmail.com>
2022-06-07 22:47:02 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,8 +7,18 @@
 */
 void print_to_98(int n)
 {
-for (n = 0; n <= 98; n++)
-{
+if (n >= 98)
+{      
+while (n > 98)
+printf("%d, ", n--);  
 printf("%d\n", n);
 }
+else   
+{
+while (n < 98)
+printf("%d, ", n++);
+printf("%d\n", n);    
 }
+}
+
+
