7d8d99d1535b2b7bf4214c5b30d66f57e415cc08
Meti-dereje <derejemeti@gmail.com>
2022-06-07 22:25:13 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,7 +2,7 @@
 /**
 * add - Adds two integers.
 * @num1: The first integer to be added.
-* @num2: The second integer to be added
+* @num2: The second integer to be added.
 *
 */
 int add(int num1, int num2)
