45f2443844c8c6a035b30eab836552fab179b9bb
Meti-dereje <derejemeti@gmail.com>
2022-06-20 04:51:58 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,10 +11,21 @@
 
 void *_memset(void *s, int c, size_t n)
 {
-unsigned int i;
-for (i = 0; i < n; i++)
+unsigned int bytes = 0;
+int index;
+while (*s)
 {
-*(s + i) = b;
+for (index = 0; accept[index]; index++)
+{
+if (*s == accept[index])
+{
+bytes++;
+break;
 }
-return (s);
+else if (accept[index + 1] == '\0')
+return (bytes);
+}
+s++;
+}
+return (bytes);
 }
