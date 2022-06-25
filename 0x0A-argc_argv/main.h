82c757600e8b70ce3b67a7d5f256188961742c46
Meti-dereje <derejemeti@gmail.com>
2022-06-25 05:44:24 -0700

    main.h

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -6,10 +6,7 @@
 char *create_array(unsigned int size, char c);
 
 char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
-int **alloc_grid(int width, int height);
-char *argstostr(int ac, char **av);
-char**strtow(char *str);
-int **alloc_grid(int width, int height);
-void free_grid(int **grid, int height);
+int _putchar(char c);
+int is_integer(const char *str);
+int get_coin_number(int amount, int nb_coins);
 #endif
