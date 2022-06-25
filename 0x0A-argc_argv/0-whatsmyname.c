e5e977861bc770a18f910e95ef248a00d41e1b5c
Meti-dereje <derejemeti@gmail.com>
2022-06-25 05:03:31 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* program prints name
+*argc :number of argument
+*argv:array that containthe argument
+*/
+int main(int argc,char *argv[])
+{
+printf("%/n", argv[0];
+(void)argc;
+return (0);
+}
