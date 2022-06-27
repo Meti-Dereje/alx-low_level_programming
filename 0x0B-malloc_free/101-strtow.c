d45db673a461b7c7ec5f670cb06c9c3272aa6c21
Meti-dereje <derejemeti@gmail.com>
2022-06-27 06:23:42 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -26,7 +26,7 @@ return (len);
 
 /**
 * count_words - Counts the number of words contained within a string.
- @str: The string to be searched.
+* @str: The string to be searched.
 *
 * Return: The number of words contained within str.
 */
